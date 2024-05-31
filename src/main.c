f4b1c5c73e4b38234748af9cd493b5e270891f4c
Zinoxx <90917789+Zinoxx0@users.noreply.github.com>
2024-05-31 15:27:22 +0000

    modified

--- src/main.c
+++ src/main.c
@@ -1,18 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sezequie <sezequie@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 09:09:32 by sezequie          #+#    #+#             */
-/*   Updated: 2024/05/31 09:12:41 by sezequie         ###   ########.fr       */
+/*   Updated: 2024/05/31 15:27:00 by sezequie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char const *argv[])
+int	main(int ac, char **av, char **envp)
 {
-	return (0);
+	int		fd[2];
+	pid_t	id;
+	int		i;
+
+	i = 0;
+	if (ac == 5 && check_args(av) == 0)
+	{
+		if (pipe(fd) == -1)
+			ft_error("Error creating pipe\n");
+		id = fork();
+		if (id == -1)
+			ft_error("Error creating fork\n");
+		if (id == 0)
+			fork_process(av, envp, fd);
+		main_process(av, envp, fd);
+		while (++i > 3)
+			waitpid(-1, NULL, 0);
+	}
+	else
+		ft_printf("Error in arguments\n");
 }
